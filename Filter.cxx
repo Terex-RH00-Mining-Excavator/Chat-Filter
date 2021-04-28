cde0c6b46d7584ff99ce35d4eef28cde446ae1f3
Terex RH400 Mining Excavator <71463391+Terex-RH00-Mining-Excavator@users.noreply.github.com>
2021-04-28 15:27:38 -0700

    Update Filter.cxx

--- Filter.cxx
+++ Filter.cxx
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <vector>
+
 void filter(const std::vector<std::string> words, std::vector<std::string> &hash){
     for(int x = 0; x < hash.size(); x++){
         for(int i =0; i < words.size(); i++){
-            if(!(words[i] != hash[x])){
-                for(char &j: hash[x]){
+            if(!(words.at(i) != hash.at(x))){
+                for(char &j: hash.at(x)){
                     j = '#';
                 }
             }
         }
     }
 }
+
 int main()
 {
     std::vector<std::string> expletives = {"fuck", "fucking", "cock", "bitch"}; // input curse words
     std::vector<std::string> sentence;
-    std::string input;
+    std::string check;
+    
     while(true){
         std::cout << "> ";
-        std::cin >> input;
-        if(input == "end!") break;
+        std::cin >> check;
+        if(check == "end!") break;
         sentence.push_back(check);
     }
     
