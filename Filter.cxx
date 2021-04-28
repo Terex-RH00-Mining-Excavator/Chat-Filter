f22c663dd9fbe1fc58120e13bc940a6f46fa9cfe
Terex RH400 Mining Excavator <71463391+Terex-RH00-Mining-Excavator@users.noreply.github.com>
2021-04-28 15:25:22 -0700

    Update Filter.cxx

--- Filter.cxx
+++ Filter.cxx
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <vector>
-
-std::vector<std::string> filter(std::vector<std::string> words, std::vector<std::string> hash){
+void filter(const std::vector<std::string> words, std::vector<std::string> &hash){
     for(int x = 0; x < hash.size(); x++){
         for(int i =0; i < words.size(); i++){
-            if(words[i] == hash[x]){
+            if(!(words[i] != hash[x])){
                 for(char &j: hash[x]){
                     j = '#';
                 }
             }
         }
     }
-    return hash;
 }
 int main()
 {
     std::vector<std::string> expletives = {"fuck", "fucking", "cock", "bitch"}; // input curse words
     std::vector<std::string> sentence;
-    std::string check;
-    
+    std::string input;
     while(true){
         std::cout << "> ";
-        std::cin >> check;
-        if(check == "end!") break;
+        std::cin >> input;
+        if(input == "end!") break;
         sentence.push_back(check);
     }
     
+    filter(expletives, sentence);
+    
     std::cout << "Output: ";
-    std::vector<std::string> FF = filter(expletives, sentence);
-    for(std::string i: FF){
+    for(std::string i: sentence){
         std::cout << i << " ";
     }
 }
